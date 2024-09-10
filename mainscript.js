9844099d8e48a7c9b27c6b69774a326add29fc41
bug0630 <bugbdh@gmail.com>
2024-09-10 18:08:15 +0900

    highlight color

--- mainscript.js
+++ mainscript.js
@@ -76,23 +76,19 @@ jQuery(document).ready(function () {
     }
   });
   function toggleLnbOnSmallScreen() {
-    if (window.matchMedia("(max-width: 768px)").matches) {
-      document.querySelectorAll(".fnb h5").forEach(function (h5) {
-        h5.addEventListener("click", function () {
-          const lnb = this.nextElementSibling; // h5의 다음 형제 요소인 lnb 선택
-          const currentDisplay = window.getComputedStyle(lnb).display;
-
-          // display가 'none'이면 'block'으로, 그렇지 않으면 'none'으로 설정
-          if (currentDisplay === "none") {
-            lnb.style.display = "block";
-          } else {
-            lnb.style.display = "none";
-          }
+    if ($(window).width() <= 768) {
+      $(".fnb h5")
+        .off("click")
+        .on("click", function () {
+          let $lnb = $(this).next(); // h5의 다음 형제 요소인 lnb 선택
+          $lnb.toggle(); // display가 'none'이면 'block'으로, 그렇지 않으면 'none'으로 설정
         });
-      });
+    } else {
+      $(".fnb h5").off("click"); // 화면 크기가 768px 이상일 때 클릭 이벤트 제거
     }
   }
+
   // 초기화 시 및 화면 크기 변경 시 실행
   toggleLnbOnSmallScreen();
-  window.addEventListener("resize", toggleLnbOnSmallScreen);
+  $(window).on("resize", toggleLnbOnSmallScreen);
 });
