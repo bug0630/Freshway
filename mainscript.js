30bddd683192537459a805388524c572f3d3e2a4
bug0630 <bugbdh@gmail.com>
2024-09-18 16:55:25 +0900

    position

--- mainscript.js
+++ mainscript.js
@@ -132,7 +132,7 @@ jQuery(document).ready(function () {
         });
       }
     });
-  $(".top_btn").click(function () {
+  $("#top").click(function () {
     $("html, body").animate({ scrollTop: 0 }, "smooth");
   });
 });
